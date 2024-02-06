247842885c4c2a89fbadacecba46053adb9a1237
MoazAlaa7 <moazalaa4727@gmail.com>
2024-02-06 22:23:32 +0200

    Handling GET Requests

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server!' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    resuls: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
