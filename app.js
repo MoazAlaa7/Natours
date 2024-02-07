5ad203baa9bc3d0a0928c810187137bd6e9d8f5e
MoazAlaa7 <moazalaa4727@gmail.com>
2024-02-07 22:59:34 +0200

    Refactoring file structure: creating routes, controllers, and server

--- app.js
+++ app.js
@@ -1,166 +1,18 @@
 const express = require('express');
-const fs = require('fs');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
-// Middleware
+
 app.use(express.json());
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toDateString();
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// Route Handlers
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    resuls: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  const tour = tours[req.params.id];
-  //if (req.params.id >= tours.length || req.params.id < 0) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  const tour = tours[req.params.id];
-  //if (req.params.id >= tours.length || req.params.id < 0) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const updatedTour = { ...tour, ...req.body };
-  tours[req.params.id] = updatedTour;
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: updatedTour,
-        },
-      });
-    }
-  );
-};
-
-const deleteTour = (req, res) => {
-  const tour = tours[req.params.id];
-  //if (req.params.id >= tours.length || req.params.id < 0) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const updatedTours = tours.filter((t) => t.id !== tour.id);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(updatedTours),
-    (err) => {
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
-    }
-  );
-};
-
-const getAllUsers = (req, res) => {
-  res.status(501).json({
-    status: 'error',
-    message: 'This route is not yet Implemented',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(501).json({
-    status: 'error',
-    message: 'This route is not yet Implemented',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(501).json({
-    status: 'error',
-    message: 'This route is not yet Implemented',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(501).json({
-    status: 'error',
-    message: 'This route is not yet Implemented',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(501).json({
-    status: 'error',
-    message: 'This route is not yet Implemented',
-  });
-};
-
 // Routers
-const tourRouter = express.Router();
-const userRouter = express.Router();
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// Routes
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
