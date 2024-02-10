053bf7b3869d971c2b66218757b1de9f867c6f89
MoazAlaa7 <moazalaa4727@gmail.com>
2024-02-11 00:40:41 +0200

    Setting up mongoose, creating documents, and testing model

--- server.js
+++ server.js
@@ -1,6 +1,49 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'a tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 3,
+  },
+  price: {
+    type: Number,
+    required: [true, 'a tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 1500,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log(`Error: ${err.message}`));
 
 const port = 3000;
 app.listen(port, () => {
