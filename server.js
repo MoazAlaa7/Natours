b5e4f87c4ed19be47de2385e8dfd192431e8100d
MoazAlaa7 <moazalaa4727@gmail.com>
2024-02-11 03:15:07 +0200

    Refactoring for MVC Architecture

--- server.js
+++ server.js
@@ -17,34 +17,6 @@ mongoose
   })
   .then(() => console.log('Connected to MongoDB'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'a tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 3,
-  },
-  price: {
-    type: Number,
-    required: [true, 'a tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 1500,
-});
-
-testTour
-  .save()
-  .then((doc) => console.log(doc))
-  .catch((err) => console.log(`Error: ${err.message}`));
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
