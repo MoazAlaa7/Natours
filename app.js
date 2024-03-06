701ef0d5247d239accb6330d8dbd9e2d0c643a66
MoazAlaa7 <moazalaa4727@gmail.com>
2024-03-06 09:04:12 +0200

    Handling unhandled routes

--- app.js
+++ app.js
@@ -14,5 +14,11 @@ app.use((req, res, next) => {
 // Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'The requested URL was not found on this server.',
+  });
+});
 
 module.exports = app;
