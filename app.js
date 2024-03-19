04b367faad176ade28e52c6fe2656f052651c9a5
MoazAlaa7 <moazalaa4727@gmail.com>
2024-03-20 00:17:47 +0200

    Integrate express-mongo-sanitize

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -19,6 +20,9 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toDateString();
