9b88ab513fc20d201f8451a3d63787f7e1a577a4
MoazAlaa7 <moazalaa4727@gmail.com>
2024-03-20 00:42:25 +0200

    Prevent Parameter Pollution

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,8 +11,10 @@ const errorController = require('./controllers/errorController');
 
 const app = express();
 
+// Add security HTTP headers
 app.use(helmet());
 
+// Limit API requests
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -19,10 +22,25 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+// parse request body
 app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toDateString();
