a8fed214de1b9ed84acfbcc0017a3fa1b7260e4d
MoazAlaa7 <moazalaa4727@gmail.com>
2024-04-08 01:31:59 +0200

    Added reviews router

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
 const AppError = require('./utils/appError');
 const errorController = require('./controllers/errorController');
 
@@ -50,6 +52,8 @@ app.use((req, res, next) => {
 // Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+
 app.all('*', (req, res, next) => {
   next(new AppError('The requested URL was not found on this server.', 404));
 });
