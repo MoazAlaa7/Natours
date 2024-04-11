9ef7d0a7efdddd393680a28aa2dbef2fea787076
MoazAlaa7 <moazalaa4727@gmail.com>
2024-04-11 22:26:18 +0200

    Utilize controllerFactory for Getting document/s

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,21 +1,5 @@
 const Review = require('../models/reviewModel');
-const APIFeatures = require('../utils/apiFeatures');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./controllerFactory');
-
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = req.params.tourId
-    ? await Review.find({ tour: req.params.tourId })
-    : await Review.find();
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
 
 exports.setTourUserIds = (req, res, next) => {
   req.body = {
@@ -27,6 +11,8 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
